521f47c40aeba01d9a2a3403380fced6b92d192b
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2022-05-15 14:01:10 +0200

    ajout session expired apres 5min

--- frontend/src/_helpers/jwtInterceptor.js
+++ frontend/src/_helpers/jwtInterceptor.js
@@ -25,11 +25,9 @@ jwtInterceptor.interceptors.request.use(request => {
 })
 
 
-//let token = localStorage.getItem('token');
-//let decoded = jwt_decode(token);
-//console.log(decoded);
 //------------------------------------------------------------------------//
-//response interceptor pour rafraichir le token sans le délais de 5 minute
+//response interceptor pour rafraichir le token avec délais de 5 minute
+
 jwtInterceptor.interceptors.response.use(response => {
 
     return response
@@ -80,112 +78,4 @@ jwtInterceptor.interceptors.response.use(response => {
 );
 
 export default jwtInterceptor
-
-//-----Intercepteur reponse Api pour traiter l'erreur
-
-/*jwtInterceptor.interceptors.response.use(response => {
-    console.log('dans intercept res');
-    console.log(response);
-    console.log(localStorage.getItem('refresh'));
-    return response
-}, error => {
-    //const refreshToken = localStorage.getItem('refresh')
-
-    if (error.response.status == 401) {
-
-
-        try {
-            let refreshToken = localStorage.getItem("refresh")
-            const rs = jwtInterceptor.post("/auth/refresh",
-                refreshToken
-            )
-
-            const { accessToken } = rs.data
-
-            localStorage.setItem("token", accessToken)
-
-
-            // const { access_Token } = rs.data
-
-            // localStorage.setItem("token", access_Token)
-
-        } catch (err) {
-            return Promise.reject(err)
-        }
-    } else {
-        localStorage.removeItem('token')
-        localStorage.removeItem('refresh')
-        //tokenService.logout()
-        router.push('/')
-        alert('la session a expiré')
-    }
-
-})
-//let refreshToken = localStorage.getItem('refresh')
-var decodedToken = localStorage.getItem('token');
-if (decodedToken.exp < new Date() / 1000) {
-    console.log("EXPIRED");
-}*/
-
-/*jwtInterceptor.interceptors.response.use((response) => {
-    return response
-}, async function (error) {
-    const originalRequest = error.config;
-    if (error.config.url != "/auth/refresh" && error.response.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-        const refreshToken = localStorage.getItem('refresh')
-
-        if (refreshToken && refreshToken != "") {
-            jwtInterceptor.defaults.headers.common['Authorization'] = `Bearer ${refreshToken}`;
-            console.log(refreshToken);
-            await jwtInterceptor.post("/auth/refresh").then((response) => {
-                // TODO: mettre à jour l'accessToken dans le localStorage
-                originalRequest.headers['Authorization'] = `Bearer ${response.data.access_token}`;
-                jwtInterceptor.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`;
-            }).catch((err) => {
-                console.log(err.response.status)
-                console.log(err);
-                refreshToken = null;
-            });
-            return instance(originalRequest);
-        }
-    }
-    return Promise.reject(error);
-});*/
-
-
-/*jwtInterceptor.interceptors.response.use(response => {
-
-    return response
-}, error => {
-
-    const originalRequest = error.config
-    console.log(originalRequest);
-
-    if (error.response.status === 401) {
-
-        try {
-            let refreshToken = localStorage.getItem("refresh")
-            console.log(refreshToken);
-
-
-            axios.post("http://localhost:8888/auth/refresh", refreshToken).then((res) => {
-                console.log(res).catch(err => console.log(err))
-            })
-
-            return axios(originalRequest)
-
-        } catch (err) {
-            return Promise.reject(err)
-        }
-
-    } else {
-        localStorage.removeItem('token')
-        localStorage.removeItem('refresh')
-    }
-    return Promise.reject(error)
-})*/
-
-
-
 
